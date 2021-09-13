2d3aac6ba86ffe543e0f21ff60db4c4cd7e87aeb
Mona <emonearth915@gmail.com>
2021-09-12 21:21:34 -0400

    flashcard_app

--- src/App.js
+++ src/App.js
@@ -22,7 +22,31 @@ function App() {
   }, [])
 
    useEffect (() => {
-    
+    axios
+    .get('https://opentdb.com/api.php', {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
+    .then(res => {
+       setFlashcards(res.data.results.map((questionItem, index) => {
+         const answer = decodeString(questionItem.correct_answer)
+         const options = [
+           ...questionItem.incorrect_answers.map(a=> decodeString(a)
+           ), 
+           answer]
+
+         return {
+           id: `${index}-${Date.now()}`,
+           question: decodeString(questionItem.question),
+           answer: answer,
+           options: options.sort(() => Math.random() - .5)
+
+         }
+       }))
+   
+      })
    }, [])
 
    function decodeString(str) {
--- src/FlashcardList.js
+++ src/FlashcardList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Flashcard from './Flashcard'
 
 export default function FlashcardList({ flashcards }) {
